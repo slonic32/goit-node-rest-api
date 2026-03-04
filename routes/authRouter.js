8dcaf03aae32051ed68adcf646625575a8ac77b6
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2026-03-04 17:28:23 +0200

    Revert "Hw05 avatars"

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,10 +4,8 @@ import {
   loginUser,
   logoutUser,
   registerUser,
-  updateAvatar,
 } from "../controllers/authControllers.js";
 import { auth } from "../middlewares/authMiddleware.js";
-import { uploadImage } from "../middlewares/imgMiddleware.js";
 
 const authRouter = express.Router();
 
@@ -18,7 +16,5 @@ authRouter.post("/login", loginUser);
 authRouter.post("/logout", auth, logoutUser);
 
 authRouter.get("/current", auth, currentUser);
-
-authRouter.patch("/avatars", auth, uploadImage, updateAvatar);
 
 export default authRouter;
